964587006c0ae75a44d81ab3df682bec8898355f
Ilya Protsenko <ilya.protsenko@nixsolutions.com>
2024-03-25 12:11:59 +0000

    Added some options to cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,POST,DELETE',
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(3001);
 }
